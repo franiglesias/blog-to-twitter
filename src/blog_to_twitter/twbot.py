a68773e97d5978dbd25252c16b41d148fdc8277b
Fran Iglesias <franiglesias@mac.com>
2021-12-11 18:00:31 +0100

    Twitter configuration management

--- src/blog_to_twitter/twbot.py
+++ src/blog_to_twitter/twbot.py
@@ -2,9 +2,9 @@ import tweepy
 
 
 class TwBot:
-    def __init__(self, configuration):
-        auth = tweepy.OAuthHandler(configuration.consumer_key(), configuration.consumer_secret())
-        auth.set_access_token(configuration.access_token(), configuration.access_token_secret())
+    def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret):
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
 
         self.api = tweepy.API(auth)
 
